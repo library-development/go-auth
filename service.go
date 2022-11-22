28c2b37d0a99f98b33683a335e28a536e16f2e40
Michael Rybka <merybka@gmail.com>
2022-11-21 21:14:44 -0500

    wip

--- service.go
+++ service.go
@@ -7,11 +7,4 @@ type Service struct {
 }
 
 func (s *Service) SignIn(username, password string) (*ui.User, error) {
-	if user, ok := s.Users[username]; ok {
-		if user.Password == password {
-			return user, nil
-		}
-	}
-
-	return nil, ErrInvalidCredentials
 }
