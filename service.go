0c61bf35650bc3765db1992b782a1870289e985d
Michael Rybka <merybka@gmail.com>
2022-11-22 20:24:39 -0500

    wip

--- service.go
+++ service.go
@@ -4,6 +4,11 @@ import "github.com/schema-cafe/go-types/ui"
 
 type Service struct {
 	Users map[string]*ui.User
+	Apps  map[string]*struct {
+		ID           string
+		PasswordHash string
+		APIKeys      map[string]bool
+	}
 }
 
 func (s *Service) SignIn(username, password string) (*ui.User, error) {
