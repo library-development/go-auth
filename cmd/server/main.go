5840458154cb6b2b24548552b6a6273a1015ce4b
Michael Rybka <merybka@gmail.com>
2022-11-23 15:31:35 -0500

    Handle CORS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 
 	"github.com/library-development/go-auth"
+	"github.com/library-development/go-web"
 )
 
 var db auth.Service
@@ -36,79 +37,81 @@ func main() {
 	lock := sync.Mutex{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		lock.Lock()
-		defer lock.Unlock()
+		web.HandleCORS(w, r)
+		if r.Method == http.MethodPost {
+			lock.Lock()
+			defer lock.Unlock()
+			switch r.URL.Path {
+			case "/cmd/admin/add-invite-token":
+				token := db.AddInviteToken()
+				json.NewEncoder(w).Encode(token)
+			case "/cmd/admin/remove-invite-token":
+				var input struct {
+					Token string `json:"token"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				db.RemoveInviteToken(input.Token)
+			case "/cmd/user/sign-up":
+				var input struct {
+					Email       string `json:"email"`
+					Password    string `json:"password"`
+					InviteToken string `json:"inviteToken"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				err := db.SignUp(input.Email, input.Password, input.InviteToken)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				}
+			case "/cmd/user/sign-in":
+				var input struct {
+					Email    string `json:"email"`
+					Password string `json:"password"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				token, err := db.SignIn(input.Email, input.Password)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				}
+				json.NewEncoder(w).Encode(token)
+			case "/cmd/user/sign-out":
+				var input struct {
+					Email string `json:"email"`
+					Token string `json:"token"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				err := db.SignOut(input.Email, input.Token)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				}
+			case "/cmd/user/change-password":
+				var input struct {
+					Email    string `json:"email"`
+					Token    string `json:"token"`
+					Password string `json:"password"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				err := db.ChangePassword(input.Email, input.Token, input.Password)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				}
+			case "/admin/verify-token":
+				var input struct {
+					Email string `json:"email"`
+					Token string `json:"token"`
+				}
+				json.NewDecoder(r.Body).Decode(&input)
+				ok := db.VerifyToken(input.Email, input.Token)
+				json.NewEncoder(w).Encode(ok)
+			}
 
-		switch r.URL.Path {
-		case "/cmd/admin/add-invite-token":
-			token := db.AddInviteToken()
-			json.NewEncoder(w).Encode(token)
-		case "/cmd/admin/remove-invite-token":
-			var input struct {
-				Token string `json:"token"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			db.RemoveInviteToken(input.Token)
-		case "/cmd/user/sign-up":
-			var input struct {
-				Email       string `json:"email"`
-				Password    string `json:"password"`
-				InviteToken string `json:"inviteToken"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			err := db.SignUp(input.Email, input.Password, input.InviteToken)
+			b, err := json.Marshal(db)
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
+				panic(err)
 			}
-		case "/cmd/user/sign-in":
-			var input struct {
-				Email    string `json:"email"`
-				Password string `json:"password"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			token, err := db.SignIn(input.Email, input.Password)
+			err = os.WriteFile(dataFile, b, 0644)
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
+				panic(err)
 			}
-			json.NewEncoder(w).Encode(token)
-		case "/cmd/user/sign-out":
-			var input struct {
-				Email string `json:"email"`
-				Token string `json:"token"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			err := db.SignOut(input.Email, input.Token)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-		case "/cmd/user/change-password":
-			var input struct {
-				Email    string `json:"email"`
-				Token    string `json:"token"`
-				Password string `json:"password"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			err := db.ChangePassword(input.Email, input.Token, input.Password)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-		case "/admin/verify-token":
-			var input struct {
-				Email string `json:"email"`
-				Token string `json:"token"`
-			}
-			json.NewDecoder(r.Body).Decode(&input)
-			ok := db.VerifyToken(input.Email, input.Token)
-			json.NewEncoder(w).Encode(ok)
-		}
-
-		b, err := json.Marshal(db)
-		if err != nil {
-			panic(err)
-		}
-		err = os.WriteFile(dataFile, b, 0644)
-		if err != nil {
-			panic(err)
 		}
 	})
 
